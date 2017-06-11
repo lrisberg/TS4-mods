748bb13ac55d95a0de453860fe2c98aa41145cb6
Lauren <lauren.risberg@gmail.com>
2017-06-10 18:19:18 -0600

    function reads files from a directory and searches all of them ffor relevant matches

--- veganize.py
+++ veganize.py
@@ -2,23 +2,27 @@ import shutil
 import os.path
 import re
 
-def copy_files_with_replace(src, filenames, dest):
+def copy_files_with_replace(src, replacements, dest):
     if os.path.exists(dest):
         shutil.rmtree(dest)
     os.makedirs(dest)
-    for (filename, regex, subst) in filenames:
+    filenames = os.listdir(src)
+    for filename in filenames:
         src_path = os.path.join(src, filename)
         dest_path = os.path.join(dest, filename)
-        with open (src_path, "r") as myfile:
-            data = myfile.read()
+        with open(src_path, "r") as in_file:
+            data = in_file.read()
+        for (regex, subst) in replacements:
             result = re.sub(regex, subst, data, 0)
             if result:
-                with open (dest_path, "w") as outfile:
-                    outfile.write(result)
+                data = result
+        with open(dest_path, "w") as out_file:
+            out_file.write(data)
+            out_file.close()
 
-food_filenames = [
-    ('EB97F823!00000000!0000000000007EB8.recipe_Food_Homestyle_EggsToast-Single.RecipeTuning.xml', r"0xA70D623E[^>]*>", '0x3D94FDEF<!--Avocado Toast-->')
+food_replacements = [
+    (r"0xA70D623E[^>]*>", '0x3D94FDEF<!--Avocado Toast-->')
 ]
 
 if __name__ == "__main__":
-    copy_files_with_replace('SampleInputs', food_filenames, 'Outputs')
+    copy_files_with_replace('SampleInputs', food_replacements, 'Outputs')
